39fcf871e82d8dc3036841ca0d4e2342c777e073
Raouf Rahiche <mrrahicheraouf@gmail.com>
2025-04-20 20:05:01 +0100

    remove comments from the test

--- test/blur_hash_widget_test.dart
+++ test/blur_hash_widget_test.dart
@@ -6,7 +6,6 @@ void main() {
   testWidgets('BlurHash widget basic smoke test', (WidgetTester tester) async {
     const String testHash = 'LGF5]+Yk^6#M@-5c,1J5@[or[Q6.';
 
-    // Build a simple widget with just the BlurHash
     await tester.pumpWidget(
       MaterialApp(
         home: Scaffold(
@@ -23,10 +22,8 @@ void main() {
       ),
     );
 
-    // Verify the BlurHash widget exists
     expect(find.byType(BlurHash), findsOneWidget);
 
-    // Check the BlurHash properties
     final BlurHash blurHash = tester.widget(find.byType(BlurHash));
     expect(blurHash.hash, equals(testHash));
     expect(blurHash.image, isNull);
